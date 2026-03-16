c32f2a11af87e2b19d5e163fea780cd61fcc18f2
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-16 16:29:20 +1100

    feat: add Card component to display data from pokemon api and handle logic to handle the score

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+export default function Card({ card, score, setScore, highestScore, setHighestScore, clickedCards, setClickedCards }) {
+    function handleClick() {
+        if (clickedCards.includes(card.id)) {
+            setHighestScore(prev => Math.max(prev, score))
+            setScore(0)
+            setClickedCards([])
+        } else {
+            setScore(prev => prev + 1)
+            setClickedCards(prev => [...prev, card.id])
+        }
+    }
+
+    return (
+        <div className="card" onClick={handleClick}>
+            <img src={card.image} alt={card.id} />
+            <p>{card.name}</p>
+        </div>
+    )
+}
